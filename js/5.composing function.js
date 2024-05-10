2d86d185260b6f7f1b097d315c7fea16e3f72713
uzairahmed2126 <uzairahmed21261@gmail.com>
2024-05-10 20:17:29 +0530

    composing function

--- js/5.composing function.js
+++ js/5.composing function.js
@@ -1,13 +1,20 @@
-function writeMessage(message, salutation, name) {
-    return `<p style="padding:3em;font-family:monospace">${message}<br><br>${salutation},<br>${name}</p>`;
-}
-document.write(writeMessage('Hey pal!', 'Party at my place tonight', 'bring snacks!'));
+// const add = x => x + 1;
+// const multiplyByTwo = x => x * 2;
 
-const add = function (x) {
-    return x + 1;
-}
-const multiply = function (x) {
-    return x * 2;
-}
-const composedFunction = function (x) { return multiply(add(x)) };
-console.log(composedFunction(3)); // Output: 8
+// // Function composition using the compose function
+// const compose = (f, g) => x => f(g(x));
+
+// const addThenMultiplyByTwo = compose(multiplyByTwo, add);
+
+// console.log(addThenMultiplyByTwo(5)); // Output: 12 (5 + 1 = 6, 6 * 2 = 12)
+
+// const multiplyByTwo = (x) => x * 2;
+// const add = (x) => x + 1;
+// const compose = (f, g) => (x) => f(g(x));
+// const addThenMultiplyByTwo = compose(add, multiplyByTwo);
+// console.log(addThenMultiplyByTwo(5));
+
+const table = (start = 1, end = 10) => start * end;
+const compose = (f) => (x) => f(x);
+const value = compose(table);
+console.log(value(1));
